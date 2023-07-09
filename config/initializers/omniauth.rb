0bedc57818eeb44f71b0d97ac2563b6816b0182f
DahuiSheng <sheng.131216@gmail.com>
2023-07-09 13:04:10 +0900

    環境変数を設定

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.applicatioin.config.middleware.use OmniAuth::Builder do
     if Rails.env.development? || Rails.env.test?
-        provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
+        provider :github, ENV["YOUR_CLIENT_ID"], ENV["YOUR_CLIENT_SECRET"]
     else
         provider :github,
             Rails.application.credentials.github[:client_id],
